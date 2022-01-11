a1c500b119cc84c06d466c4016381bd9ae9455f7
Adhitya Tri <adhityatriwahyu@gmail.com>
2022-01-11 11:20:27 +0700

    clear project

--- components/layouts/Navbar.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const NavWrapper = styled.div`
-    background-color: ${props => props.theme.colors.primary};
-    border: 1px dashed orange;
-    text-align: center;
-    font-weight: bold;
-    padding: ${props => props.theme.space};
-`
