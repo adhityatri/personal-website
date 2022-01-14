87901d9d26a288557f1d7bce660403056946f73a
adhitya tri <adhityatriwahyu@gmail.com>
2022-01-14 07:46:54 +0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,20 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `http://adhityatri.skom.id/`,
+  siteMetadata: {
+    siteUrl: `http://adhityatri.skom.id/`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Heebo`,
+          `Signika Negative\:400,500,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
     },
-    plugins: [
-        {
-            resolve: `gatsby-plugin-google-fonts`,
-            options: {
-                fonts: [
-                    `Heebo`,
-                    `Signika Negative\:400,500,700` // you can also specify font weights and styles
-                ],
-                display: 'swap'
-            }
-        }
-    ]
-}
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+};
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,70 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { StaticImage } from "gatsby-plugin-image";
 
 const HeroWrapper = styled.div`
-    border: 2px solid white;
-    height: 250px;
-    border-radius: ${props => props.theme.space};
-    background: ${props => props.theme.colors.primary};
-`
+  border: 2px solid white;
+  height: 200px;
+  border-radius: ${(props) => props.theme.space};
+  background: rgba(255, 255, 255, 0.2);
+  display: flex;
+  overflow: hidden;
+`;
+
+const GreethingTitle = styled.h1`
+  margin: 0;
+`;
+const GreethingDescription = styled.p`
+  margin: 0;
+`;
+
+const Greething = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+  overflow: hidden;
+  padding: ${(props) => props.theme.space};
+  flex: 1;
+  color: ${(props) => props.theme.colors.text};
+`;
+
+const ImageWrapper = styled.div`
+  position: absolute;
+  top: 5%;
+  bottom: 0;
+  right: 0;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    right: 5%;
+  }
+  border-top-left-radius: 50px;
+  opacity: 0.9;
+`;
 
 function Hero() {
-    return (
-        <HeroWrapper>
+  return (
+    <HeroWrapper>
+      <Greething>
+        <>
+          <GreethingTitle>Hello,</GreethingTitle>
+          <GreethingDescription>
+            I'm a frontend developer based in Indonesia!
+          </GreethingDescription>
+        </>
 
-        </HeroWrapper>
-    )
+        <ImageWrapper>
+          <StaticImage
+            src="../images/me.png"
+            alt="A dinosaur"
+            placeholder="blurred"
+            height={250}
+            width={200}
+          />
+        </ImageWrapper>
+      </Greething>
+    </HeroWrapper>
+  );
 }
 
-export default Hero
+export default Hero;
