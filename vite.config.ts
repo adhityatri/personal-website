eb049ba84a7c7b1a982d2299425064b744090175
adhityatriwahyu <adhityatriwahyu@gmail.com>
2024-06-18 16:51:35 +0000

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'personal-website',
   server: {
     host: true,
     port: 8080,
