db103f2856490a342b283f6eaf428ff1a0a924af
adhityatriwahyu <adhityatriwahyu@gmail.com>
2024-06-18 17:10:05 +0000

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'personal-website',
   server: {
     host: true,
     port: 8080,
