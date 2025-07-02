65c5b180362120547955a90e2c1553e7d2258b28
Adhitya Tri <adhitya.tri@gramedia.id>
2025-07-02 13:20:56 +0700

    fix brand source

--- components/app/Brand.vue
+++ components/app/Brand.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center gap-2">
     <nuxt-link to="/" class="text-[#006d77] font-bold">
-      <img src="/logo.svg" height="32" width="32" alt="logo" />
+      <img src="/Logo.svg" height="32" width="32" alt="logo" />
     </nuxt-link>
   </div>
 </template>
